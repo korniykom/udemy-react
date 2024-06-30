b8f733a3e32c83a92efa3df76214df009bdc938e
Maksym Korniiko <korniykom@gmail.com>
2024-06-30 15:01:10 +0300

    feat: complete challenge text-expander

---
+++ React/Part 2/challenge-text-expander/src/App.js
@@ -0,0 +1,66 @@
+import "./index.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
+
+function TextExpander({
+  children,
+  collapsedNumWords = 5,
+  buttonColor = "#f2f",
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+}) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  function toggleOpen() {
+    setIsCollapsed(!isCollapsed);
+  }
+
+  const buttonStyle = {
+    color: `${buttonColor}`,
+    backgroundColor: "transparent",
+    border: "none",
+  };
+
+  return (
+    <div className="box">
+      {isCollapsed
+        ? `${children.split(" ").slice(0, collapsedNumWords).join(" ")}...`
+        : children}
+      <button style={buttonStyle} onClick={toggleOpen}>
+        {isCollapsed ? expandButtonText : collapseButtonText}
+      </button>
+    </div>
+  );
+}
